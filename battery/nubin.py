453309570984cecba8308c6620cb8a12c869025c
Deju35 <107037555+Deju35@users.noreply.github.com>
2022-06-08 15:12:45 +0300

    Create nubin.py

---
+++ battery/nubin.py
@@ -0,0 +1,8 @@
+from battery.battery import Battery
+from utils import add_years_to_date
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
